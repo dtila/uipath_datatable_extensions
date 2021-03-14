c02661f622d12e3e075c25a4573dcfed9d34ef9b
dtila <contact@dtila.ro>
2021-03-14 12:48:40 +0200

    Add ReadColumnNameByColumnIndex.cs activity + tests + test files

---
+++ src/DataTableExtensions.Tests/ReadColumnNameByColumnIndexTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace DataTableExtensions.Tests
+{
+    public class ReadColumnNameByColumnIndexTests
+    {
+        public readonly ReadColumnNameByColumnIndex _activity = new ReadColumnNameByColumnIndex();
+
+
+        [Theory]
+        [InlineData("Excels\\Current Asset Report.xlsx", 1, "MER_F1_dce2934d4c05c88274b599e9")]
+        public void WhenFileIsValid_ColumnNameIsReadCorrectly(string excelPath, int position, string expectedColumnName)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                { nameof(ReadColumnNameByColumnIndex.WorkbookPath), excelPath},
+                { nameof(ReadColumnNameByColumnIndex.SheetIndex), position },
+            };
+
+            var properties = WorkflowInvoker.Invoke(_activity, parameters);
+            var sheetName = (string)properties[nameof(ReadColumnNameByColumnIndex.SheetName)];
+
+            sheetName.Should().Be(expectedColumnName);
+        }
+    }
+}
---
+++ src/DataTableExtensions/ReadColumnNameByColumnIndex.cs
@@ -0,0 +1,36 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTableExtensions
+{
+    public class ReadColumnNameByColumnIndex : CodeActivity
+    {
+        [RequiredArgument]
+        [Category("Input")]
+        public InArgument<string> WorkbookPath { get; set; }
+
+        [Category("Input")]
+        public InArgument<int> SheetIndex { get; set; }
+
+        [Category("Output")]
+        public OutArgument<string> SheetName { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            using (var workbook = new XLWorkbook(WorkbookPath.Get(context)))
+            {
+                var workSheet = workbook.Worksheet(SheetIndex.Get(context));
+                if (workSheet == null)
+                    throw new InvalidOperationException("Worksheet is not in a valid range");
+
+                SheetName.Set(context, workSheet.Name);
+            }
+        }
+    }
+}
